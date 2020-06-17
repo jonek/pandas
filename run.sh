3f9762995be96698588875f8adc934cdb7ff1f08
Jonek <jonek@gmx.net>
2020-06-17 21:30:09 +0200

    Create possibility to specify a port and container name
    This allows for multiple pandas containers to run at the same time.
    Problem: the stop.sh script only works when stopping the default one.

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-LOCAL_PORT=8080
+if [[ -z ${PORT+x} ]]; then
+    PORT=8080
+fi
+if [[ -z ${CONTAINER_NAME+x} ]]; then
+    CONTAINER_NAME=pandas
+fi
 if [[ -z ${DATA_DIR+x} ]]; then
     DATA_DIR=$(pwd)/data
 fi
@@ -12,18 +17,18 @@ else
 fi
 
 docker run --detach \
-    --publish ${LOCAL_PORT}:8888 \
+    --publish ${PORT}:8888 \
     --volume ${DATA_DIR}:/data \
     --volume ${NOTEBOOK_DIR}:/notebooks \
-    --name pandas \
+    --name ${CONTAINER_NAME} \
     pandas start-notebook.sh --notebook-dir=/notebooks --notebookApp.token=12345
 
 echo; echo -n "."; sleep 1; echo -n "."; sleep 1; echo -n ". "
 
-TOKEN=$(docker logs pandas 2>&1 | grep "?token=" | cut -d "=" -f 2 | uniq)
-URL=http://localhost:${LOCAL_PORT}/notebooks/${NOTEBOOK}?token=${TOKEN}
+TOKEN=$(docker logs ${CONTAINER_NAME} 2>&1 | grep "?token=" | cut -d "=" -f 2 | uniq)
+URL=http://localhost:${PORT}/notebooks/${NOTEBOOK}?token=${TOKEN}
 
 echo "opening URL in browser:"
 echo "${URL}"
 
-xdg-open ${URL}
+xdg-open "${URL}"
